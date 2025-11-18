ecca16f4dcc0dab2c2fbdcebdde0ffe1a706aa61
bobisaperson1 <bfulham@bradyfulham.com>
2025-11-18 12:33:31 +1030

    conifg flow

--- custom_components/Philips_SICP/config_flow.py
+++ custom_components/Philips_SICP/config_flow.py
@@ -1,5 +1,6 @@
 from homeassistant import config_entries
 from .const import DOMAIN
+import voluptuous as vol
 
 class ExampleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Example config flow."""
@@ -7,3 +8,9 @@ class ExampleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     # Home Assistant will call your migrate method if the version changes
     VERSION = 1
     MINOR_VERSION = 1
+    async def async_step_user(self, info):
+        if info is not None:
+            pass  # TODO: process info
+        return self.async_show_form(
+            step_id="user", data_schema=vol.Schema({vol.Required("password"): str})
+        )
